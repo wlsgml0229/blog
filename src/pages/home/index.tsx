298c7466fa5b315501d9be4b0ec8fd6cc9f4547e
wlsgml0229 <wlsgml0229@naver.com>
2023-10-17 23:07:22 +0900

    Feat: 게시글 UI 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,11 +9,33 @@ export default function Home() {
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-      <div className="post-list"> Post List</div>
+      <div className="post__navigation">
+        <div className="post__navigation--active">전체</div>
+        <div>나의글</div>
+      </div>
+      <div className="post__list">
+        {[...Array(10)].map((e, idx) => (
+          <div key={idx} className="post__box">
+            <Link to={`/posts/${idx}`}>
+              <div className="post__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">김지닝</div>
+                <div className="post__date">2023.10.10 월요일</div>
+              </div>
+              <div className="post__title">게시글 {idx}</div>
+              <div className="post__text">rptlrmfrp</div>
+              <div className="post__utils-box">
+                <div className="post__delete">삭제</div>
+                <div className="post__edit">수정</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <div>Menu 1</div>
-        <div>Menu 2</div>
-        <div>Menu 3</div>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
       </footer>
     </div>
   );

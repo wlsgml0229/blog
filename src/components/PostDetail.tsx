ab41c31954302eac7f099ff8e45521400e6ff6cb
wlsgml0229 <wlsgml0229@naver.com>
2023-10-18 22:05:30 +0900

    Feat: 게시글 페이지 UI 추가

---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,21 @@
+export default function PostDetail() {
+  return (
+    <>
+      <div className="post__detail">
+        <div className="post__box">
+          <div className="post__title">Lorem ipsum dolor sit amet</div>
+          <div className="post__profile-box">
+            <div className="post__profile" />
+            <div className="post__author-name">김지닝</div>
+            <div className="post__date">2023.10.10 월요일</div>
+          </div>
+          <div className="post__utils-box">
+            <div className="post__delete">삭제</div>
+            <div className="post__edit">수정</div>
+          </div>
+          <div className="post__text">rptlrmfrp</div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,3 +1,13 @@
-export default function PostDetail() {
-  return <div>PostDetail</div>;
+import PostDetail from "../../components/PostDetail";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+export default function PostPage() {
+  return (
+    <>
+      <Header />
+      <PostDetail />
+      <Footer />
+    </>
+  );
 }

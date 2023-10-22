d25b16e1c00f03e0ceac6921ca94d673c4f2d8b2
wlsgml0229 <wlsgml0229@naver.com>
2023-10-22 13:46:12 +0900

    config 경로 단축 설정

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-import Home from "../pages/home";
-import PostList from "../pages/posts";
-import PostDetail from "../pages/posts/detail";
-import PostNew from "../pages/posts/new";
-import PostEdit from "../pages/posts/edit";
-import Profile from "../pages/profile";
-import Login from "../pages/login";
-import SignUp from "../pages/signup";
+import Home from "pages/home";
+import PostList from "pages/posts";
+import PostDetail from "pages/posts/detail";
+import PostNew from "pages/posts/new";
+import PostEdit from "pages/posts/edit";
+import Profile from "pages/profile";
+import Login from "pages/login";
+import SignUp from "pages/signup";
 
 export default function Router() {
   return (
--- src/pages/posts/new.tsx
+++ src/pages/posts/new.tsx
@@ -1,3 +1,12 @@
+import Header from "components/Header";
+import Footer from "components/Footer";
+
 export default function PostNew() {
-  return <div>PostNew</div>;
+  return (
+    <>
+      <Header />
+      <div></div>
+      <Footer />
+    </>
+  );
 }

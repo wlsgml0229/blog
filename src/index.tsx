ae442a1f33ecf6992f9eb316cb5d63f940fb2da9
wlsgml0229 <wlsgml0229@naver.com>
2023-10-22 16:59:04 +0900

    Feat: firebase 설치 및 설정추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter as Router} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import firebase from "firebaseApp";
 
+console.log(firebase);
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-      <Router>
-          <App />
-      </Router>
-  </React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
 );
-

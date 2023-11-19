f7f0cc33993733b6f95e9cb2fc3c696b26ea9e73
wlsgml0229 <wlsgml0229@naver.com>
2023-11-19 17:13:19 +0900

    Feat: 글쓰기 기능추가

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,17 +1,86 @@
+import React, { useContext, useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import AuthContext from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 export default function PostForm() {
+  const [title, setTitle] = useState<string>("");
+  const [summary, setSummary] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await addDoc(collection(db, "posts"), {
+        title: title,
+        summary: summary,
+        content: content,
+        createAt: new Date()?.toLocaleDateString(),
+        email: user?.email,
+      });
+      toast.success("게시글을 등록했습니다");
+      navigate("/");
+    } catch (e: any) {
+      console.log(e);
+      toast.error(e?.code);
+    }
+  };
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const {
+      target: { name, value },
+    } = e;
+    switch (name) {
+      case "title":
+        setTitle(value);
+        break;
+      case "summary":
+        setSummary(value);
+        break;
+      case "content":
+        setContent(value);
+        break;
+      default:
+        break;
+    }
+  };
   return (
-    <form action="/post" method="POST" className="form">
+    <form onSubmit={onSubmit} className="form">
       <div className="form__block">
         <label htmlFor="title">제목</label>
-        <input type="text" name="title" id="title" required />
+        <input
+          type="text"
+          name="title"
+          id="title"
+          required
+          onChange={onChange}
+          value={title}
+        />
       </div>
       <div className="form__block">
         <label htmlFor="title">요약</label>
-        <input type="text" name="summary" id="summary" required />
+        <input
+          type="text"
+          name="summary"
+          id="summary"
+          required
+          onChange={onChange}
+          value={summary}
+        />
       </div>
       <div className="form__block">
         <label htmlFor="title">내용</label>
-        <textarea name="content" id="content" required />
+        <textarea
+          name="content"
+          id="content"
+          required
+          onChange={onChange}
+          value={content}
+        />
       </div>
       <div className="form__block">
         <button type="submit" value="제출" className="form__btn--submit">
--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, FirebaseApp, getApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 export let app: FirebaseApp;
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -18,7 +16,6 @@ try {
 } catch (e) {
   app = initializeApp(firebaseConfig, "app");
 }
-// Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
-
+export const db = getFirestore(app);
 export default firebase;
